de0602a22fcae58088b8eaa8187fc11e0e8e978f
tamazs <majortamas313@gmail.com>
2021-08-25 11:14:12 +0200

    done

--- index.php
+++ index.php
@@ -5,7 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP styling</title>
 </head>
-<body>
-    
+<body style="background-color: <?php echo $_POST["background-color"]; ?>; color: <?php echo $_POST["font-color"]; ?>; font-family: <?php echo $_POST["font"]; ?>;">
+    <form action="index.php" method="post">
+        <label>Background color:</label>
+        <input type="text" name="background-color" value="<?php echo $_POST["background-color"] ?>">
+
+        <label>Font color:</label>
+        <input type="text" name="font-color" value="<?php echo $_POST["font-color"] ?>">
+
+        <label>Font name:</label>
+        <input type="text" name="font" value="<?php echo $_POST["font"] ?>">
+
+        <button>Preview</button>
+    </form>
 </body>
 </html>
